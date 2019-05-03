563c75692ee9a269b58a1087a1eb1e260db9dc88
joshstern1 <jstern19@bu.edu>
2019-05-03 15:39:19 -0400

    updated hcsr04

--- hcsr04.c
+++ hcsr04.c
@@ -1,4 +1,6 @@
-//Hoop Central Project - Ultrasonic Sensor
+//Hoop Central Project
+//Device Drvier for HC-SR04 Ultrasonic Sensor
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -40,12 +42,6 @@ struct file_operations hcsr04_fops = {
 };
 
 
-
-//red = echo
-//green = trigger
-//white = vcc
-//grey = ground
-
 //interrupt handlers
 static irqreturn_t echo_rising_handler(int irq, void *dev_id);
 static irqreturn_t echo_falling_handler(int irq, void *dev_id);
@@ -60,32 +56,39 @@ static int score_read( char *page, char **start, off_t off, int count, int *eof,
 static struct timer_list my_timer;
 
 //variables used for counting
-static unsigned int score;
-static unsigned int counting;
+static unsigned int score; //running score of baskets made
+static unsigned int counting; //1 - start detecting baskets, 0 - stop
 static unsigned int TRIGGER_PIN;
 static unsigned int ECHO_PIN_UP;
 static unsigned int ECHO_PIN_DOWN;
-static unsigned int valid;
+static unsigned int valid; //used to ensure that the rising handler is called before the falling handler
 static unsigned int got;
-static unsigned int timeout_len;
+static unsigned int timeout_len; //used for when the sensor times out, timeouts usually mean error
 
 //variables used for taking timestamps
-static struct timeval trigger_time;
-static struct timeval echo_time;
+static struct timeval trigger_time; //used for getting time of day
+static struct timeval echo_time; //used for getting time of day
 
 static void send_pulse(void);
 
 static void my_timer_callback() {
+	//timer has gone off
+	//if got == 0, it means the sensor timed out
 	if ((got == 0) && (counting == 1)){
-		//printk(KERN_INFO "Timeout\n");
+		//this likely means the sensor is not working and should be debugged
+		printk(KERN_INFO "Timeout\n");
 	}
 	got = 0;
+	//
 	send_pulse();
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //echo pin interrupt handlers
+//once we send a pulse from the trigger pin, we wait on the echo to receive a high signal
+//the duration of this high signal tells us how far away an object is
 
+//the triggered ultrasonic signal has returned
 static irqreturn_t echo_rising_handler(int irq, void *dev_id) {
 
 	if((counting == 1) && (valid == 1)){
@@ -97,6 +100,7 @@ static irqreturn_t echo_rising_handler(int irq, void *dev_id) {
 	return IRQ_HANDLED;
 }
 
+//the triggered ultrasonic signal has completed its return and has dropped back to a low signal
 static irqreturn_t echo_falling_handler(int irq, void *dev_id) {
 	unsigned long duration;
 
@@ -105,17 +109,18 @@ static irqreturn_t echo_falling_handler(int irq, void *dev_id) {
 		//caluclate how much time in milliseconds occurred between the rising handler and falling handler being called
 		duration = (echo_time.tv_sec - trigger_time.tv_sec) * 1000000 + (echo_time.tv_usec - trigger_time.tv_usec);
 		if(duration < 1500){
+			//for debugging
 			printk(KERN_INFO "duration = %lu\n", duration);
 		}
 		got = 1; //sensor detected an object before the timeout occurred
 		if(duration < 1200){
 			//if basket was made, increment score
 			score++;
-			//start a counter for 1 second to repeat the distance detection
+			//start a counter for 0.5 seconds second to repeat the distance detection
 			mod_timer(&my_timer, jiffies + msecs_to_jiffies(500));
 		}
 		else{
-			//start a counter for 0.25 seconds to repeat the distance detection
+			//start a counter for 50 milliseconds to repeat the distance detection
 			mod_timer(&my_timer, jiffies + msecs_to_jiffies(50));
 		}
 	}
@@ -130,7 +135,13 @@ static int hcsr04_init(void) {
 	int result, ret;
 	result = register_chrdev(hcsr04_major, "hcsr04", &hcsr04_fops);
 
-	score = -2;
+	score = -2; //start at -2 so that the speaker program knows the game is at the starting screen
+	/*
+	score = -2 -> start screen
+	score = -1 -> end screen
+	score >= 0 -> number of baskets that have been made
+	*/
+
 	counting = 0; //0 - reset score to 0, 1 - count score, should start as 0
 	valid = 1; //1 - sensor ready to detect distance, 0 - sensor in the middle of a distance collection
 	got = 0;
@@ -150,12 +161,11 @@ static int hcsr04_init(void) {
 
 	//set interrupt handlers for the input echo signal
 	ret = request_irq(gpio_to_irq(ECHO_PIN_UP), echo_rising_handler, IRQF_TRIGGER_RISING, "echo_rising_handler", NULL);
-  	ret = request_irq(gpio_to_irq(ECHO_PIN_DOWN), echo_falling_handler, IRQF_TRIGGER_FALLING, "echo_falling_handler", NULL);
+  ret = request_irq(gpio_to_irq(ECHO_PIN_DOWN), echo_falling_handler, IRQF_TRIGGER_FALLING, "echo_falling_handler", NULL);
 
 	setup_timer(&my_timer, my_timer_callback, 0);
+	//start timer
 	mod_timer(&my_timer, jiffies + msecs_to_jiffies(500));
-
-	//send_pulse();
 
 	return 0;
 }
@@ -174,6 +184,7 @@ static int hcsr04_release(struct inode *inode, struct file *filp) {
 	return 0;
 }
 static ssize_t hcsr04_read(struct file *filp, char *buf, size_t count, loff_t *f_pos) {
+	//don't need to have any read functionality, as we have our lcd and speaker programs read from /proc/hcsr04
 	return count;
 }
 
@@ -187,12 +198,14 @@ static ssize_t hcsr04_write(struct file *filp, const char *buf, size_t count, lo
 		printk(KERN_INFO "invalid input\n");
 		return count;
 	}
-	
+
 	if (buf[0] == '1'){
+		//reset score, start counting
 		score = 0;
 		counting = 1;
 	}
 	else if (buf[0] == '2'){
+		//stop counting, reset score
 		score = 0;
 		counting = 0;
 	}
@@ -204,7 +217,7 @@ static ssize_t hcsr04_write(struct file *filp, const char *buf, size_t count, lo
 		score = -2;	//start screen
 		counting = 0;
 	}
-	printk(KERN_INFO "counting = %d, score = %d\n", counting, score);
+	//printk(KERN_INFO "counting = %d, score = %d\n", counting, score);
 	return count;
 }
 
@@ -222,6 +235,7 @@ static void send_pulse(){
 		pxa_gpio_set_value(TRIGGER_PIN, 0);
 	}
 	else{
+		//else if we're not counting, keep timer going at 500 milliseconds so that it will still be ready for when the driver needs to start counting again
 		mod_timer(&my_timer, jiffies + msecs_to_jiffies(500));
 	}
 }
